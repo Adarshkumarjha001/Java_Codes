3d834e47eca4357c0ff33e77ad8e19c26b613465
Adarsh Kumar Jha <117103488+Adarshkumarjha001@users.noreply.github.com>
2024-01-15 23:39:02 +0530

    01_intro.java

---
+++ 01_intro.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+        int c=13;
+        System.out.println(a+b+"Hellow"+a+b+c);
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        String s1=sc.nextLine();
+        System.out.println(x+y+s1);
+    }
+}
